609193579d7b8c30c3c3eb3d2aa49fb15a3b5119
ronenbet <33380705+ronenbet@users.noreply.github.com>
2018-09-02 00:22:05 +0300

    ~

--- Audio_test.py
+++ Audio_test.py
@@ -1,65 +1,38 @@
-
 import pyaudio
-import sys
-import numpy as np
-import aubio
+import wave
 
-# initialise pyaudio
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
+
 p = pyaudio.PyAudio()
 
-# open stream
-buffer_size = 1024
-pyaudio_format = pyaudio.paFloat32
-n_channels = 1
-samplerate = 44100
-stream = p.open(format=pyaudio_format,
-                channels=n_channels,
-                rate=samplerate,
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
                 input=True,
-                frames_per_buffer=buffer_size)
+                frames_per_buffer=CHUNK)
 
-if len(sys.argv) > 1:
-    # record 5 seconds
-    output_filename = sys.argv[1]
-    record_duration = 5 # exit 1
-    outputsink = aubio.sink(sys.argv[1], samplerate)
-    total_frames = 0
-else:
-    # run forever
-    outputsink = None
-    record_duration = None
+print("* recording")
 
-# setup pitch
-tolerance = 0.8
-win_s = 4096 # fft size
-hop_s = buffer_size # hop size
-pitch_o = aubio.pitch("default", win_s, hop_s, samplerate)
-pitch_o.set_unit("midi")
-pitch_o.set_tolerance(tolerance)
+frames = []
 
-print("*** starting recording")
-while True:
-    try:
-        audiobuffer = stream.read(buffer_size)
-        signal = np.fromstring(audiobuffer, dtype=np.float32)
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
 
-        pitch = pitch_o(signal)[0]
-        confidence = pitch_o.get_confidence()
+print("* done recording")
 
-        print("{} / {}".format(pitch,confidence))
-
-        if outputsink:
-            outputsink(signal, len(signal))
-
-        if record_duration:
-            total_frames += len(signal)
-            if record_duration * samplerate < total_frames:
-                break
-    except KeyboardInterrupt:
-        print("*** Ctrl+C pressed, exiting")
-        break
-
-print("*** done recording")
 stream.stop_stream()
 stream.close()
 p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
